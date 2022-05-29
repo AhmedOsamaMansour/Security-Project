8fafb2153ebfb270f892efca7557d471446541b6
AhmedOsamaMansour <100098722+AhmedOsamaMansour@users.noreply.github.com>
2022-05-29 22:37:31 +0200

    first commit

---
+++ Security Package/startupcode/securitylibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+        // 97 Start Of Small Letter
+        // 65 Start of Capital Letter
+        // Plain Text Small Letter and Return Capital Letter
+        // ( Plain Text [i] + Key ) % 26
+        public string Encrypt(string plainText, int key)
+        {
+            string cipherText = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                cipherText += Convert.ToChar(((Convert.ToInt16(plainText[i]) - 97 + key) % 26) + 65);
+            }
+
+            return cipherText;
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            string plainText = "";
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                if ((Convert.ToInt16(cipherText[i]) - 65 - key) < 0)
+                {
+                    plainText += Convert.ToChar(((Convert.ToInt16(cipherText[i]) - 65 - key) + 26) + 97);
+                }
+                else
+                {
+                    plainText += Convert.ToChar(((Convert.ToInt16(cipherText[i]) - 65 - key) % 26) + 97);
+                }
+            }
+            return plainText;
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+
+            int key = 0;
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                // 32 Diffrenece Bettwen A (97) and a (65) Asci code 
+                key = (Convert.ToInt16(cipherText[i]) - Convert.ToInt16(plainText[i]) + 32);
+                if (key < 0)
+                {
+                    key = key + 26;
+                }
+
+            }
+            return key;
+        }
+    }
+}
---
+++ Security Package/startupcode/securitylibrary/RSA/RSA.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary.RSA
+{
+    public class RSA
+    {
+        public int PowerFunction(int Base, int power, int mod)
+        {
+            ///M^e mod n
+            /// mod <- n = p*q
+            /// Power <- e
+            /// Base <- M
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result = (result * Base) % mod;
+            }
+            return result;
+        }
+        static int gcd(int number, int b)
+        {
+            int r;
+
+            while (b != 0)
+            {
+                r = number % b;
+                number = b;
+                b = r;
+            }
+
+            return number;
+        }
+        public int GetMultiplicativeInverse(int number, int baseN)
+        {
+            if (gcd(number, baseN) != 1)
+                return -1;
+            // A1 <- x , A2 <- y , A3 <- m0
+            int m0 = baseN;
+            int y = 0, x = 1;
+
+            while (number > 1)
+            {
+                //Q <- A3/B3 , A3 <- number, B3 <- baseN
+                int q = number / baseN;
+
+                int t = baseN;
+
+                baseN = number % baseN;
+                number = t;
+                t = y;
+
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0)
+                x += m0;
+
+            return x;
+        }
+        public int Encrypt(int p, int q, int M, int e)
+        {
+            //throw new NotImplementedException();
+            int n = p * q;
+            int fie_n = (p - 1) * (q - 1);
+            //C = M^e mod n 	
+            int cipherText = PowerFunction(M, e, n);
+            return cipherText;
+
+        }
+
+        public int Decrypt(int p, int q, int C, int e)
+        {
+            //throw new NotImplementedException();
+            // M = C^d mod n
+            int n = p * q;
+            int fie_n = (p - 1) * (q - 1);
+            int d = 0;
+            if (gcd(fie_n, e) == 1)
+            {
+                d = GetMultiplicativeInverse(e, fie_n);
+            }
+            int plainText = PowerFunction(C, d, n);
+            return plainText;
+        }
+    }
+}
